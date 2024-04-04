b3cd9693b8f3428e5004a940b53458c6cee24772
justutpal <kumarutpal2112001@gmail.com>
2024-04-04 15:36:30 +0530

    Stop watch is finally done

---
+++ javascript.js
@@ -0,0 +1,60 @@
+let container = document.querySelector('.container');
+let display = document.querySelector('.display');
+let markup = document.querySelector('.markup');
+let start = document.querySelector('.start');
+let stopIt = document.querySelector('.stop');
+let reset = document.querySelector('.reset');
+let minute = document.querySelector(".min");
+let second = document.querySelector(".sec");
+let miliSecond = document.querySelector(".mili");
+let hour = document.querySelector(".hour");
+let par = document.querySelector('.par');
+let isClicked = 'false'
+let sec = 0;
+let min = 0;
+let hor = 0;
+let mili = 0;
+let clear = ""
+
+markup.addEventListener('click', () => {
+    if (second.innerHTML > 0) {
+    let paras = document.createElement('p');
+    paras.innerText = hor + " : " + min + " : " + sec + " : " + mili;
+    container.appendChild(paras)
+    }
+})
+
+start.addEventListener('click', () => {
+    start.style.display = 'none'
+    stopIt.style.display = 'inline'
+    clear = setInterval(() => {
+        mili++
+        miliSecond.innerHTML = mili
+
+        if(miliSecond.innerHTML == 99) {
+        mili = 0
+        sec++
+        second.innerHTML = `${sec}`
+        }
+        if (second.innerHTML == 60) {
+            sec = 0
+            min++
+            minute.innerHTML = min
+            if (minute.innerHTML == 60) {
+                min = 0
+                hor++
+                hour.innerHTML = hor
+            }
+        }
+    }, 10)
+})
+
+stopIt.addEventListener('click', () => {
+    start.style.display = 'inline'
+    stopIt.style.display = 'none'
+    clearInterval(clear)
+})
+
+reset.addEventListener('click', () => {
+    location.reload()
+})
